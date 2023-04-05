94feb824ba8d7f4fde54d3241ee68fb24f52ae93
Ajay Bhattacharyya <AjayBhatta16@github.com>
2023-04-05 04:42:01 -0400

    basic results screen

--- client/src/components/results/ResultsScreen.js
+++ client/src/components/results/ResultsScreen.js
@@ -2,10 +2,29 @@ import React from "react"
 import Header from "../common/Header"
 
 export default function ResultsScreen(props) {
+    const getFraudString = (score) => {
+        if(score < 25) {
+            return "This account has a very low fraud risk"
+        } 
+        if(score < 50) {
+            return "This account has a fairly low fraud risk"
+        }
+        if(score < 75) {
+            return "This account has a moderate fraud risk"
+        } 
+        return "This account has a high fraud risk"
+    }
     return (
         <>
             <Header/>
-            <h1>Results</h1>
+            <div className="container mt-5 pt-3 pb-5 bg-dark text-white">
+                <div className="text-center">
+                    <h1 className="font-weight-light text-underline mb-4">Scan Results</h1>
+                </div>
+                <h2>Fraud risk: {props.results.score}%</h2>
+                <h6>{getFraudString(props.results.score)}</h6>
+                <h2 className="mt-5">Scam Type: {props.results.scamType}</h2>
+            </div>
         </>
     )
 }
