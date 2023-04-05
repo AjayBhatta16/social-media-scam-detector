4a9ee7005c20a11fcc7dc067f45fccbccb38081f
Ajay Bhattacharyya <AjayBhatta16@github.com>
2023-04-05 04:10:54 -0400

    Home screen

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Header() {
     return (
         <>
-            <nav className='navbar navbar-expand-md bg-dark navbar-dark shadow-md px-1'>
+            <nav className='navbar navbar-expand-md bg-dark navbar-dark shadow-md px-1 mb-5'>
                 <Link to='/'>
                     <a href='#' className='navbar-brand py-0 px-0 mx-0'>
                         <img src='logo.png' alt='logo' height={40}/>
--- client/src/components/home/HomeScreen.js
+++ client/src/components/home/HomeScreen.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../common/Header'
 
 export default function HomeScreen(props) {
+    const headStyle = {
+        color: '#0fdce3',
+        fontFamily: 'monospace'
+    }
+    const btnStyle = {
+        color: '#ccc',
+        backgroundColor: 'darkblue',
+        borderRadius: '20%/50%'
+    }
+    const [errTxt, setErrTxt] = useState('')
+    const urlRef = useRef()
+    const navigate = useNavigate()
+    const handleClick = () => {
+        if(!urlRef.current.value || urlRef.current.value.length == 0) {
+            setErrTxt("Please enter a social media profile URL")
+            return 
+        }
+        props.setResults({
+            score: 75,
+            scamType: 'Account Recovery'
+        })
+        navigate('results')
+    }
     return (
         <>
             <Header/>
-            <h1>Home</h1>
+            <div className='container mt-5 pt-3 text-center'>
+                <h1 className='mt-5' style={{...headStyle}}>Social Media Scam Detector</h1>
+                <h6 className='mt-3' style={{...headStyle, fontWeight: '100'}}>
+                    Enter the URL of a social media profile to scan it for fraud
+                </h6>
+                <input ref={urlRef} className='mt-5 mb-3 w-75 p-2' placeholder='Profile URL' type="text" />
+                <br/><small className='text-danger'>{errTxt}</small><br/>
+                <button onClick={handleClick} className='btn px-3 py-2 mt-2' style={btnStyle}>Scan Profile</button>
+            </div>
         </>
     )
 }
