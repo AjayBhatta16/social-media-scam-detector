527a8f35e0c0f2fcce2f7ee672b3de0e614536bf
Ajay Bhattacharyya <AjayBhatta16@github.com>
2023-04-05 03:30:21 -0400

    finished Header

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
-        <></>
+        <>
+            <nav className='navbar navbar-expand-md bg-dark navbar-dark shadow-md px-1'>
+                <Link to='/'>
+                    <a href='#' className='navbar-brand py-0 px-0 mx-0'>
+                        <img src='logo.png' alt='logo' height={40}/>
+                    </a>
+                </Link>
+            </nav>
+        </>
     )
 }
